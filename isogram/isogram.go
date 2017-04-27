3cf133ea96bf8e8ddfb2191af7db85a0104b8c9c
Nicanor Perera <nicanorperera@gmail.com>
2017-04-26 21:03:21 -0300

    IsIsogram doc.

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,9 +1,11 @@
+
 package isogram
 
 import "strings"
 
 const testVersion = 1
 
+// IsIsogram determines if a word or phrase is an isogram.
 func IsIsogram(s string) bool {
 	m := map[rune]bool{}
 	s = strings.ToLower(s)
